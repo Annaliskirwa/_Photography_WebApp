1baeef542b87db51c487cb8172d23699d9be9f8f
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-28 21:10:42 +0300

    Configuring the location urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     url('^$',views.index,name = 'index'),
     url(r'^search/', views.search_image, name='search_image'),
+    url(r'^location/(?P<image_location>\d+)', views.location_filter, name='location_filter'),
 ]
