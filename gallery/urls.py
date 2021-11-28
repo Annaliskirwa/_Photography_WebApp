7dfdcf37a4d2c5502dbf80f928b40e44caad07fd
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-28 21:11:22 +0300

    Configuring the images url

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     url('^$',views.index,name = 'index'),
     url(r'^search/', views.search_image, name='search_image'),
     url(r'^location/(?P<image_location>\d+)', views.location_filter, name='location_filter'),
+    url(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.single,name = 'single')
 ]
