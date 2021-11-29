52d35dd8c1b51ae626aa1fb2f10d7ccce50cb719
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-29 18:48:10 +0300

    Configuring the url settings

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
+from django.conf import settings
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
@@ -7,3 +9,5 @@ urlpatterns=[
     url(r'^location/(?P<image_location>\d+)', views.location_filter, name='location_filter'),
     url(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.single,name = 'single')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
