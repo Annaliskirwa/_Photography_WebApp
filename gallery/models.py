c381be5d225030fcaa9cf6959469db827e3f87f6
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-29 19:28:50 +0300

    Added ondelete on location and categories

--- gallery/models.py
+++ gallery/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+# from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
+
+
 class Location(models.Model):
     name = models.CharField(max_length =50)
 
@@ -26,6 +29,7 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
+
 class Category(models.Model):
     name = models.CharField(max_length =50)
 
@@ -47,13 +51,14 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
 class Image(models.Model):
     name = models.CharField(max_length = 60)
     pic = models.ImageField(upload_to = 'uploads/')
     # picture = ImageField( blank = True, manual_crop = '1920x1080')
     description = models.TextField()
-    image_location = models.ForeignKey('Location')
-    image_category = models.ForeignKey('Category')
+    image_location = models.ForeignKey('Location',on_delete = models.CASCADE)
+    image_category = models.ForeignKey('Category',on_delete = models.CASCADE)
 
     def save_image(self):
         self.save()
